f98bf8460f9325c68edaa0aa92eabbdfc5c01e1a
jarltuxen <jarl.tuxen@gmail.com>
2024-04-30 14:46:05 +0200

    Initial commit

---
+++ src/main/java/dk/kea/bootstrapdemo/BootstrapdemoApplication.java
@@ -0,0 +1,13 @@
+package dk.kea.bootstrapdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/bootstrapdemo/BootstrapdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.bootstrapdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
